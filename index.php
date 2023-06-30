7679491879ef8c1803b058cc561341b1830c8a7a
Arthurgdct <grandclementarthur@gmail.com>
2023-06-30 19:46:30 +0200

    Ajout de la majeur partie du html et création des fichiers

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Assets/Css/index.css">
+    <script src="https://kit.fontawesome.com/72e8ccb00a.js" crossorigin="anonymous"></script>
+    <title>Portfolio Agc</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="#">Agc</a>
+            <i></i>
+        </nav>
+    </header>
+
+    <main>
+        <section>
+            <h1>Portfolio</h1>
+            <h2>Arthur<br>Grand-clement</h2>
+            <h2>Développeur Web</h2>
+        </section>
+        <section>
+            <h2>Présentation</h2>
+            <img src="Assets/Img/1661325227976.jpg" alt="Photo de moi">
+            <div>
+                <p>Je m’appel Arthur, j’ai 26 ans et je suis développeur web junior.</p>
+                <p>Après avoir suivi une formation et obtenu un dipôme de développeur web, web mobile, j'ai développé un éventail de compétences et accumulé une grande quantité de connaissances en participant à divers projets qui impliquaient l'utilisation de différents langages de programmation.</p>
+                <p>Conscient de la nécessité de continuer à progresser, je suis extrêmement déterminé à poursuivre mon apprentissage afin de me perfectionner.</p>
+            </div>
+        </section>
+        <section>
+            <div>
+                <i class="fa-regular fa-hourglass-half"></i>
+                <p>Patience & détermination</p>
+            </div>
+            <div>
+                <i class="fa-solid fa-user-group-simple"></i>
+                <p>Collaboratiion & esprit d'équipe</p>
+            </div>
+            <div>
+                <i class="fa-solid fa-book" style="color: #ffffff;"></i>
+                <p>Curiosité & soif de connaissance</p>
+            </div>
+            <div>
+                <i class="fa-solid fa-shuffle"></i>
+                <p>Flexibilité & sens de l'organisation</p>
+            </div>
+        </section>
+        <section>
+            <h2>Portfolio</h2>
+        </section>
+        <section>
+            <a href="">Telécharger mon cv</a>
+            <i class="fa-solid fa-file-arrow-down"></i>
+        </section>
+        <section>
+            <h2>Contact</h2>
+            <form action="" method="post">
+                <div>
+                    <input type="text" id="name" name="user_name" placeholder="Nom & prénom*">
+                </div>
+                <div>
+                    <input type="email" id="email" name="user_email" placeholder="Adresse email*">
+                </div>
+                <div>
+                    <input type="text" id="object" name="user_object" placeholder="Objet*">
+                </div>
+                <div>
+                    <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                </div>
+                <input type="button" value="Envoyer mon message">
+            </form>
+        </section>
+    </main>
+    <footer>
+        <p>© Grand-clement Arthur | 2023</p>
+        <i class="fa-brands fa-linkedin" style="color: #ffffff;"></i>
+        <i class="fa-brands fa-github" style="color: #ffffff;"></i>
+    </footer>
